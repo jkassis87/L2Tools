dfa28e08d0ed8d3bcd7e213c10a6ee95137a1368
root <root@host.jkassis.com.au>
2020-07-28 12:22:50 +0000

    Added bare login form, moved title to route file

--- app/routes.py
+++ app/routes.py
@@ -4,20 +4,30 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
+    pagesettings = {'title': 'Index Page'}
     return render_template('index.html')
 
 
 @app.route('/r1check')
 def r1check():
-    return render_template('r1check.html')
+    
+    return render_template('r1check.html', 
+        title='R1soft Connection Checker')
 
 
 @app.route('/diskcheck')
 def diskcheck():
-    return render_template('diskcheck.html', title='Disk Usage Check')
+    return render_template('diskcheck.html',
+        title='Disk Usage Check')
 
 
 @app.route('/wpcheck')
 def wpcheck():
-    return render_template('wpcheck.html', title='WP Attack Check')
+    return render_template('wpcheck.html',
+        title='WP Attack Check')
+        
+@app.route('/timecheck')
+def timecheck():
+    return render_template('timecheck.html',
+        title='Find what happend around a certain time')
 
