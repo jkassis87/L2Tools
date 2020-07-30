46688685feefd5c814810b482485ac651ce0a324
root <root@host.jkassis.com.au>
2020-07-30 11:21:46 +0000

    custom forms added for all tools, custom command tool, ssh spur demo under disk check

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
+import spur
 
 @app.route('/')
 @app.route('/index')
@@ -8,26 +9,43 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/r1check')
+@app.route('/r1check', methods=['GET', 'POST'])
 def r1check():
-    
     return render_template('r1check.html', 
         title='R1soft Connection Checker')
 
 
-@app.route('/diskcheck')
+@app.route('/diskcheck', methods=['GET', 'POST'])
 def diskcheck():
+    if request.method == 'POST':
+        sshhost = request.form.get('sshhost')
+        sshuser = request.form['sshuser']
+        sshpass = request.form['sshpass']
+        sshport = request.form['sshport']
+        shell = spur.SshShell(hostname=sshhost, 
+                        username=sshuser, 
+                        password=sshpass,
+                        port = sshport,
+                        missing_host_key=spur.ssh.MissingHostKey.accept)
+        with shell:
+            result = shell.run(["uptime"])
+            return(result.output)
     return render_template('diskcheck.html',
         title='Disk Usage Check')
 
 
-@app.route('/wpcheck')
+@app.route('/botwpcheck', methods=['GET', 'POST'])
 def wpcheck():
-    return render_template('wpcheck.html',
-        title='WP Attack Check')
+    return render_template('botwpcheck.html',
+        title='Bot & WP Attack Check')
         
-@app.route('/timecheck')
+@app.route('/timecheck', methods=['GET', 'POST'])
 def timecheck():
     return render_template('timecheck.html',
         title='Find what happend around a certain time')
+        
+@app.route('/customcommand', methods=['GET', 'POST'])
+def customcommand():
+    return render_template('customcommand.html',
+        title='Custom Command')
 
