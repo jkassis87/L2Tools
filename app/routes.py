c64ccf5fc8ac234809e74170abb533cd15c4bf32
root <root@host.jkassis.com.au>
2020-08-03 09:10:44 +0000

    wpbotcheck tool part working, wp-login and xmlrpc results returned as json

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template, request, make_response
+from flask import render_template, request, make_response, jsonify
 from app import app
-import spur, sys
+import spur, sys, re, geoip2.database, json
 
 @app.route('/')
 @app.route('/index')
@@ -121,16 +121,60 @@ def wpcheck():
                         port = sshport,
                         missing_host_key=spur.ssh.MissingHostKey.accept)
                         
-        #### START prepares ssh commands to run
-
-
-        #### END prepares ssh commands to run
+        with shell:
         
-        # logs in to host, runs commands, outputs results to new window
-        #with shell:
-            #result = shell.run(["uptime"])
-            #return(result.output)        
-        return render_template('timecheck_results.html')
+            #### START prepares ssh commands to run
+            
+            # lists all files larger than ssh_minfilesize under /home/ and /backup/
+            com_atkcheck = shell.run(["sh", "-c", r"less /home/*/access-logs/* | grep -i 'xmlrpc\|wp-login\|bot' |  awk '{print $1}' | sort | uniq -c | sort -n"])
+            str_atkcheck = r"less /home/*/access-logs/* | grep -i 'xmlrpc\|wp-login\|bot' |  awk '{print $1}' | sort | uniq -c | sort -n"
+            
+            #### END prepares ssh commands to run
+            
+            ### START converts the com_atkcheck results to a usable dict
+            
+            # changes com_atkcheck type from binary to multiline str
+            a_str = com_atkcheck.output.decode()
+            a_str = a_str.splitlines()
+            
+            # converts atkcheck_str from multiline str to list
+            a_str = [re.sub('     ', '', i) for i in a_str]
+            
+            # turns list into dict
+            a_dict = {k:v for k,v in (x.split(' ') for x in a_str) } 
+            
+            # swaps the key and value
+            a_dict = {v:k for (k, v) in a_dict.items()}
+            
+            # converts value from str into list
+            a_dict = {k:[v] for (k, v) in a_dict.items()}
+            
+            ### END converts the com_atkcheck results to a usable dict
+            
+            ### START add geoip data to dict
+            
+            with geoip2.database.Reader('GeoLite2-City.mmdb') as reader:
+                for k, v in a_dict.items():
+                    print(type(v))
+                    response = reader.city(k)
+                    i = response.country.name
+                    v.append(i)
+                
+            with geoip2.database.Reader('GeoLite2-ASN.mmdb') as reader:
+                for k, v in a_dict.items():
+                    print(type(v))
+                    response = reader.asn(k)
+                    i = response.autonomous_system_organization
+                    v.append(i)
+            
+            ### END add geoip data to dict
+            
+            # converts the dict to json
+            check_results = json.dumps(a_dict, indent=2, separators=(',', ':'))            
+            
+            # outputs to diskcheck_results.html and renders the ssh command results
+            #return jsonify(json.dumps(a_dict))
+            return render_template('botwpcheck_results.html',  check_results=check_results)
         
         
     return render_template('botwpcheck.html',
